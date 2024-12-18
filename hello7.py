766287b4460ed7ff6e972eecab2fe697324b28c2
Aidan Do <aidand@canva.com>
2024-12-19 08:20:45 +1100

    .

--- hello7.py
+++ hello7.py
@@ -1,8 +1,9 @@
 import os
 from llama_stack_client import LlamaStackClient
-from tools import SANDBOX_DIR
+from tools import SANDBOX_DIR, TOOLS
+import json
 
-model_id = "meta-llama/Llama-3.1-405B-Instruct-FP8"
+MODEL_ID = "meta-llama/Llama-3.1-405B-Instruct-FP8"
 
 LOOP_LIMIT = 5
 
@@ -43,3 +44,104 @@ RESET = "\033[0m"
 
 
 client = LlamaStackClient(base_url=f"http://localhost:{os.environ['LLAMA_STACK_PORT']}")
+
+review_feedback = None
+for i in range(LOOP_LIMIT):
+    print(f"{BLUE}Coder Agent - Creating Plan - Iteration {i}{RESET}")
+    if review_feedback:
+        prompt_feedback = f"""
+        One of your peers has provided the following feedback:
+        {review_feedback}
+        Please adjust the plan to address the feedback.
+
+        
+        """
+    else:
+        prompt_feedback = ""
+
+    prompt =f"""
+        Create a step by step plan to complete the task of creating a codebase that will {PROGRAM_OBJECTIVE}.
+        You have 3 different operations you can perform. create_file(path, content), update_file(path, content), delete_file(path)
+        Limit your step by step plan to only these operations per step.
+
+        Here is the codebase currently:
+        {get_codebase_contents()}
+
+        {prompt_feedback}
+        Please ensure there's a README.md file in the root of the codebase that describes the codebase and how to run it.
+        Please ensure there's a requirements.txt file in the root of the codebase that describes the dependencies of the codebase.
+        """
+    response = client.inference.chat_completion(
+        model_id=MODEL_ID,
+        messages=[
+            {"role": "system", "content": CODER_AGENT_SYSTEM_PROMPT},
+            {"role": "user", "content": prompt},
+        ],
+        response_format={
+            "type": "json_schema",
+            "json_schema": {
+                "name": "Plan",
+                "description": f"A plan to complete the task of creating a codebase that will {PROGRAM_OBJECTIVE}.",
+                "strict": True,
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "steps": {"type": "array", "items": {"type": "string"}},
+                    },
+                    "required": ["steps"],
+                    "additionalProperties": False,
+                },
+            },
+        },
+        tools=TOOLS,
+    )
+    plan = json.loads(response.choices[0].message.content)
+    for step_idx, step in enumerate(plan["steps"]):
+        print(f"{step_idx + 1}. {step}")
+    print("\n")
+
+    # Coding agent executes the plan
+    # print(f"{BLUE}Coder Agent - Executing Plan - Iteration {i}{RESET}")
+    # for step in plan["steps"]:
+    #     prompt = f"""
+    #         You have 3 different operations you can perform. create_file(path, content), update_file(path, content), delete_file(path).
+    #         Here is the codebase:
+    #         {get_codebase_contents()}
+    #         Please perform the following operation: {step}
+    #         """
+    #     response = client.chat.completions.create(
+    #         model="gpt-4o-mini",
+    #         messages=[
+    #             {"role": "system", "content": CODER_AGENT_SYSTEM_PROMPT},
+    #             {"role": "user", "content": prompt},
+    #         ],
+    #         tools=TOOLS,
+    #     )
+    #     message = response.choices[0].message
+    #     if message.content:
+    #         print("Not enough information to run tool: ", message.content)
+    #     else:
+    #         tool_call = message.tool_calls[0]
+    #         run_tool(tool_call)
+    # print("\n")
+
+    # print(f"{MAGENTA}Reviewer Agent - Reviewing Codebase - Iteration {i}{RESET}")
+    # response = client.chat.completions.create(
+    #     model="gpt-4o-mini",
+    #     messages=[
+    #         {"role": "system", "content": REVIEWER_AGENT_SYSTEM_PROMPT},
+    #         {"role": "user", "content": f"""
+    #         Here is the full codebase:
+    #         {get_codebase_contents()}
+    #         Please review the codebase and make sure it is correct.
+    #         Please provide a list of changes you would like to make to the codebase.
+    #         """},
+    #     ],
+    #     stream=True,
+    # )
+    # review_feedback = ""
+    # for chunk in response:
+    #     if chunk.choices[0].delta.content:
+    #         print(chunk.choices[0].delta.content, end="", flush=True)
+    #         review_feedback += chunk.choices[0].delta.content
+    # print("\n")
--- tools.py
+++ tools.py
@@ -3,12 +3,67 @@ import json
 
 SANDBOX_DIR = "/Users/aidand/dev/auto-llama/sandbox"
 
+TOOLS = [
+    {
+        "tool_name": "create_file",
+        "description": "Create a file with the given name and content. If there are any directories that don't exist, create them.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "path": {
+                    "type": "string",
+                    "description": "The relative path to the file to create",
+                },
+                "content": {
+                    "type": "string",
+                    "description": "The content of the file to create",
+                },
+            },
+            "required": ["path", "content"],
+        },
+    },
+    {
+        "tool_name": "update_file",
+        "description": "Update a file with the given name and content. If the file does not exist, create it.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "path": {
+                    "type": "string",
+                    "description": "The relative path to the file to update",
+                },
+                "content": {
+                    "type": "string",
+                    "description": "The content of the file to update",
+                },
+            },
+            "required": ["path", "content"],
+        },
+    },
+    {
+        "tool_name": "delete_file",
+        "description": "Delete a file with the given path. If the file does not exist, do nothing.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "path": {
+                    "type": "string",
+                    "description": "The relative path to the file to delete",
+                },
+            },
+            "required": ["path"],
+        },
+    },
+]
+
+
 def create_file(path, content):
     # Create any directories that don't exist
     os.makedirs(os.path.dirname(os.path.join(SANDBOX_DIR, path)), exist_ok=True)
     with open(os.path.join(SANDBOX_DIR, path), "w") as f:
         f.write(content)
     print(f"Created file {os.path.join(SANDBOX_DIR, path)}")
+
 
 # Does the same thing as create_file - but nice to have a separate function for updating files
 # So the LLM has the option to update files if it wants to - if that makes more sense than creating a new file
@@ -18,10 +73,13 @@ def update_file(path, content):
         f.write(content)
     print(f"Updated file {os.path.join(SANDBOX_DIR, path)}")
 
+
 def delete_file(path):
     # If the file doesn't exist, don't do anything
     if not os.path.exists(os.path.join(SANDBOX_DIR, path)):
-        print(f"Tried to delete file {os.path.join(SANDBOX_DIR, path)} but it does not exist")
+        print(
+            f"Tried to delete file {os.path.join(SANDBOX_DIR, path)} but it does not exist"
+        )
         return
     os.remove(os.path.join(SANDBOX_DIR, path))
     print(f"Deleted file {os.path.join(SANDBOX_DIR, path)}")
@@ -45,6 +103,7 @@ def run_tool(tool_call):
             return
         delete_file(arguments["path"])
 
+
 if os.path.exists(SANDBOX_DIR):
     # Clear the contents of the directory
     for item in os.listdir(SANDBOX_DIR):
@@ -53,6 +112,7 @@ if os.path.exists(SANDBOX_DIR):
             os.unlink(item_path)
         elif os.path.isdir(item_path):
             import shutil
+
             shutil.rmtree(item_path)
 else:
     os.makedirs(SANDBOX_DIR)

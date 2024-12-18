1cb374d5ec0a3802f498f18e5d068b551bf7ac56
Aidan Do <aidand@canva.com>
2024-12-19 08:09:37 +1100

    .

--- hello7.py
+++ hello7.py
@@ -6,8 +6,14 @@ client = LlamaStackClient(base_url=f"http://localhost:{os.environ['LLAMA_STACK_P
 # List available models
 models = client.models.list()
 
-for model in models:
-    print(model)
+# for model in models:
+#     print(model)
 
 model_id = "meta-llama/Llama-3.1-405B-Instruct-FP8"
 
+response = client.inference.chat_completion(
+    model_id=model_id,
+    messages=[{"role": "user", "content": "What is the capital of France?"}],
+)
+
+print(response)
